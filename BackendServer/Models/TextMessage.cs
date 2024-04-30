c28692bcdf99a7b56bf17ba2ca8901396e5e1356
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-01 02:08:01 +0400

    Added TextMessage class

---
+++ BackendServer/Models/TextMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendServer.Models
+{
+    internal class TextMessage(string sender, string content) : Message(content)
+    {
+
+        private string sender = sender;
+        public string Sender
+        {
+            get { return sender; }
+            set { sender = value; }
+        }
+
+        public override string ParseRawMessage() { return Content; }
+    }
+}
