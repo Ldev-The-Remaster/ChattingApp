9170f61fd1b96139e740a4843a6da580b92b40ef
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-01 02:08:17 +0400

    Added ServerMessage class

---
+++ BackendServer/Models/ServerMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendServer.Models
+{
+    enum MessageType
+    {
+        Info,
+        Warn,
+        Error
+    }
+
+    internal class ServerMessage(MessageType messageType, string content) : Message(content)
+    {
+        private MessageType messageType = messageType;
+        public MessageType MessageType
+        {
+            get { return messageType; }
+            set { messageType = value; }
+        }
+
+        public override string ParseRawMessage() { return Content; }
+    }
+}
