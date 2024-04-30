511c10a2fe6103074ffd7008de109481a3f7d102
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-01 01:11:33 +0400

    Added Message base class

---
+++ BackendServer/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendServer.Models
+{
+    internal abstract class Message(string content)
+    {
+        private string content = content;
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        private DateTime timestamp;
+        public DateTime Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+
+        public abstract string ParseRawMessage();
+    }
+}
