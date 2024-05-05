b5df8c2047a48b9cca189a07d66f9f8cefd6a3f8
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-06 03:01:43 +0400

    Implemented GetServerOptions function to get port

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,7 +1,49 @@
 ﻿internal class Program
 {
+    struct ServerOptions
+    {
+        public int Port;
+    }
+
     private static void Main(string[] args)
     {
+        ServerOptions serverOptions = GetServerOptions();
+        RunServer(serverOptions);
+    }
+
+    private static ServerOptions GetServerOptions()
+    {
+        ServerOptions serverOptions = new ServerOptions();
+
+        // TODO: Try getting options from CLI arguments first
+
+        while (serverOptions.Port == 0 || serverOptions.Port < 1 || serverOptions.Port > 65535)
+        {
+            Console.WriteLine("Input server port:");
+            string? portInput = Console.ReadLine();
+
+            try
+            {
+                serverOptions.Port = Convert.ToInt32(portInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input");
+                continue;
+            }
+            
+            if (serverOptions.Port < 1 || serverOptions.Port > 65535)
+            {
+                Console.WriteLine("Invalid port, input number between 1 and 65535");
+            }
+        }
+        
+        return serverOptions;
+    }
+
+    private static void RunServer(ServerOptions serverOptions)
+    {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Server should start on port {serverOptions.Port}");
     }
 }
