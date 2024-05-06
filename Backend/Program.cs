abe6b5e13085684463a7e9ff58016344c47d0c9e
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-06 04:03:12 +0400

    Moved GetServerStartupOptions to its own class

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,47 +1,14 @@
-﻿internal class Program
-{
-    struct ServerOptions
-    {
-        public int Port;
-    }
+﻿using Backend;
 
+internal class Program
+{
     private static void Main(string[] args)
     {
-        ServerOptions serverOptions = GetServerOptions();
+        ServerStartupOptions serverOptions = ServerStartupOptions.GetServerStartupOptions();
         RunServer(serverOptions);
     }
 
-    private static ServerOptions GetServerOptions()
-    {
-        ServerOptions serverOptions = new ServerOptions();
-
-        // TODO: Try getting options from CLI arguments first
-
-        while (serverOptions.Port == 0 || serverOptions.Port < 1 || serverOptions.Port > 65535)
-        {
-            Console.WriteLine("Input server port:");
-            string? portInput = Console.ReadLine();
-
-            try
-            {
-                serverOptions.Port = Convert.ToInt32(portInput);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid input");
-                continue;
-            }
-            
-            if (serverOptions.Port < 1 || serverOptions.Port > 65535)
-            {
-                Console.WriteLine("Invalid port, input number between 1 and 65535");
-            }
-        }
-        
-        return serverOptions;
-    }
-
-    private static void RunServer(ServerOptions serverOptions)
+    private static void RunServer(ServerStartupOptions serverOptions)
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine($"Server should start on port {serverOptions.Port}");
