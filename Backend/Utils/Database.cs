bc3210e5470b8aabba9812cd42406dd2df42f68f
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-07 23:48:35 +0400

    Implemented Database context class

---
+++ Backend/Utils/Database.cs
@@ -0,0 +1,22 @@
+﻿using BackendServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Backend.Utils
+{
+    public class TextMessageContext : DbContext
+    {
+        public DbSet<TextMessage> TextMessages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source=Messages.db");
+        }
+
+        public static void SetUp() 
+        {
+            TextMessageContext messageContext = new TextMessageContext();
+            messageContext.Database.EnsureCreated();
+        }
+    }
+}
