578feefbe9c7c33680e6e38b988e6c8b621ec8ea
Ahmed Hamad <60454980+ahmed-okkio@users.noreply.github.com>
2024-05-16 00:41:46 +0100

    Can now connect to WS server on join

---
+++ Frontend/Frontend.Client/Models/LoginInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Frontend.Client.Models;
+
+public class LoginInfo
+{
+    public string username { get; set; } = string.Empty;
+    public string ip { get; set; } = string.Empty;
+    public int? port { get; set; }
+}
