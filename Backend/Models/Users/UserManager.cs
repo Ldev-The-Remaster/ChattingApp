ee59de94aa7ecf12a1aba0bbf8d669e4c4c2a603
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-16 23:14:06 +0400

    UserManager definition

---
+++ Backend/Models/Users/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using WebSocketSharp;
+
+namespace Backend.Models.Users
+{
+    public static class UserManager
+    {
+        public static User[] UsersList { get; set; } = [];
+
+        // Infractions
+        public static void Mute(User user) { }
+        public static void Unmute(User user) { }
+        public static void Kick(User user) { }
+        public static void Ban(User user) { }
+        public static void Unban(User user) { }
+        public static void BanIp(string ip) { }
+        public static void UnbanIp(string ip) { }
+
+        // Connection
+        public static User Connect(WebSocket socket, string ip) { return new User(socket, ip); }
+        public static void Authenticate(WebSocket socket, string username) { }
+        public static void Disconnect(User user) { }
+
+        private static User? GetUserBySocket(WebSocket socket) { return null; }
+    }
+}
