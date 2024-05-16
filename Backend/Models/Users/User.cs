8c36223d49552119c376a0e5d8bc76b5ebb20617
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-16 22:08:24 +0400

    Implemented User class

---
+++ Backend/Models/Users/User.cs
@@ -0,0 +1,49 @@
+﻿using WebSocketSharp;
+
+namespace Backend.Models.Users
+{
+    public class User(WebSocket socket)
+    {
+        private WebSocket _socket = socket;
+        public WebSocket Socket
+        {
+            get { return _socket; }
+            set { _socket = value; }
+        }
+
+        private string _username = "";
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        private string _ip = "";
+        public string Ip
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+
+        private bool _isRegistered = false;
+        public bool IsRegistered
+        {
+            get { return _isRegistered; }
+            set { _isRegistered = value; }
+        }
+
+        private bool _isMuted = false;
+        public bool IsMuted
+        {
+            get { return _isMuted; }
+            set { _isMuted = value; }
+        }
+
+        private bool _isBanned = false;
+        public bool IsBanned
+        {
+            get { return _isBanned; }
+            set { _isBanned = value; }
+        }
+    }
+}
