3e37108c580ae2ae11c26b8975d1c8c78b49ffc6
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-23 23:25:34 +0400

    Changed UserList to List from Array, implemented UserManager.Connect()

--- Backend/Models/Users/UserManager.cs
+++ Backend/Models/Users/UserManager.cs
@@ -5,7 +5,7 @@ namespace Backend.Models.Users
 {
     public static class UserManager
     {
-        public static User[] UsersList { get; set; } = [];
+        public static List<User> UsersList { get; set; } = new List<User>();
 
         // Infractions
         public static void Mute(User user) { }
@@ -17,10 +17,15 @@ namespace Backend.Models.Users
         public static void UnbanIp(string ip) { }
 
         // Connection
-        public static User Connect(WebSocket socket, string ip) { return new User(socket, ip); }
+        public static User Connect(WebSocket socket, string ip)
+        {
+            User newUser = new User(socket, ip);
+            UsersList.Add(newUser);
+            return newUser;
+        }
+
         public static void Authenticate(WebSocket socket, string username) { }
         public static void Disconnect(User user) { }
-
         private static User? GetUserBySocket(WebSocket socket) { return null; }
     }
 }
