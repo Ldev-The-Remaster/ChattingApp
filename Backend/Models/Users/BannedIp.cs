d6352fcc47849507b5fbe38b5c945507ebbd4544
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-24 01:50:02 +0400

    Added BannedIps class and context

---
+++ Backend/Models/Users/BannedIp.cs
@@ -0,0 +1,17 @@
+﻿namespace Backend.Models.Users
+{
+    public class BannedIp
+    {
+        public int IpId { get; set; }
+        public string IpAddress { get; set; }
+
+        public BannedIp()
+        {
+            IpAddress = String.Empty;
+        }
+        public BannedIp(string ip)
+        {
+            IpAddress = ip;
+        }
+    }
+}
