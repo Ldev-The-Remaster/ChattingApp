52e4ca8989d2ec7388a7eb1b56fddfe2a037c61f
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-05-30 22:42:04 +0400

    Created logger class

---
+++ Backend/Utils/Logger.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend.Utils
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine($"{GetTimestamp()} | {message}");
+        }
+
+        public static void Chat(string sender, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(sender);
+            Console.ResetColor();
+            Console.Write($" > {message}");
+        }
+        public static void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Log(message);
+            Console.ResetColor();
+        }
+        public static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Log(message);
+            Console.ResetColor();
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Log(message);
+            Console.ResetColor();
+        }
+
+        private static string GetTimestamp()
+        {
+            return DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
