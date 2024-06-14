f75ec3e8fd5d589c1258a97bbcc8d0b561ceb1bf
defintelyNotKabi <kabiasim98@gmail.com>
2024-06-14 07:09:37 -0700

    auth

--- Frontend/Frontend.Client/Models/WebSocketService.cs
+++ Frontend/Frontend.Client/Models/WebSocketService.cs
@@ -1,94 +1,103 @@
-﻿namespace Frontend.Client.Models;
-
-using System.Net.WebSockets;
+﻿using System.Net.WebSockets;
 using System.Text;
 
-public class WebSocketService
+namespace Frontend.Client.Models
 {
-    public class Message
+    public class WebSocketService
     {
-        public string? User { get; set; }
-        public string? Content { get; set; }
-        public DateTime Timestamp { get; set; }
-    }
- 
-    private ClientWebSocket? _webSocket;
-    private Task? _receiveMessagesTask;
-    public delegate void MessagesEventHandler(Message messages);
-    public event MessagesEventHandler? OnMessageReceived;
-
-    public async Task ConnectAsync(string uri)
-    {
-        if (_webSocket == null || _webSocket.State != WebSocketState.Open)
+        public class Message
         {
-            _webSocket = new ClientWebSocket();
-            await _webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
-            _receiveMessagesTask = ReceiveMessagesAsync();
+            public string? User { get; set; }
+            public string? Content { get; set; }
+            public DateTime Timestamp { get; set; }
         }
-    }
 
-    private void OnMessage(string message)
-    {
+        private ClientWebSocket? _webSocket;
+        private Task? _receiveMessagesTask;
+        public delegate void MessagesEventHandler(Message messages);
+        public event MessagesEventHandler? OnMessageReceived;
+        private bool _isAuthenticated = false;
 
-        try
+        public bool IsAuthenticated
         {
-            var lines = message.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            get { return _isAuthenticated; }
+        }
 
-            if (lines.Length >= 6 &&
-                lines[0].StartsWith("DO SEND") &&
-                lines[1].StartsWith("FROM ") &&
-                lines[2].StartsWith("IN general-chat") &&
-                lines[3].StartsWith("AT ") &&
-                lines[4].StartsWith("WITH"))
+        public async Task ConnectAsync(string uri)
+        {
+            if (_webSocket == null || _webSocket.State != WebSocketState.Open)
             {
-                var user = lines[1].Substring(5); // "FROM user"
-                var timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(lines[3].Substring(3))).DateTime; // "AT timestamp"
-                var content = string.Join("\n", lines.Skip(5)); // Content strarts after "WITH" line
-                if (OnMessageReceived != null)
+                _webSocket = new ClientWebSocket();
+                await _webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+                _receiveMessagesTask = ReceiveMessagesAsync();
+            }
+        }
+
+        private void OnMessage(string message)
+        {
+            try
+            {
+                if (message.StartsWith("DO ACCEPT"))
                 {
-                    OnMessageReceived.Invoke(new Message
+                    _isAuthenticated = true;
+                }
+                else
+                {
+                    var lines = message.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+                    if (lines.Length >= 6 &&
+                        lines[0].StartsWith("DO SEND") &&
+                        lines[1].StartsWith("FROM ") &&
+                        lines[2].StartsWith("IN general-chat") &&
+                        lines[3].StartsWith("AT ") &&
+                        lines[4].StartsWith("WITH"))
                     {
-                        User = user,
-                        Content = content,
-                        Timestamp = timestamp
-                    });
+                        var user = lines[1].Substring(5); // "FROM user"
+                        var timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(lines[3].Substring(3))).DateTime; // "AT timestamp"
+                        var content = string.Join("\n", lines.Skip(5)); // Content starts after "WITH" line
+                        OnMessageReceived?.Invoke(new Message
+                            {
+                                User = user,
+                                Content = content,
+                                Timestamp = timestamp
+                            });
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Message format is invalid.");
+                    }
                 }
-
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Error: Message format is invalid.");
+                Console.WriteLine($"Error processing message: {e.Message}");
             }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Error processing message: {e.Message}");
-        }
-    }
 
-    private async Task ReceiveMessagesAsync()
-    {
-        var buffer = new byte[1024 * 4];
-        while (_webSocket?.State == WebSocketState.Open)
+        private async Task ReceiveMessagesAsync()
         {
-            var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            if (result.MessageType == WebSocketMessageType.Text)
+            var buffer = new byte[1024 * 4];
+            while (_webSocket?.State == WebSocketState.Open)
             {
-                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                if (message != null)
+                var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Text)
                 {
-                    OnMessage(message);
+                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    if (message != null)
+                    {
+                        OnMessage(message);
+                    }
                 }
             }
         }
-    }
 
-    public async Task SendMessageAsync(string message)
-    {
-        if (_webSocket != null && _webSocket.State == WebSocketState.Open)
+        public async Task SendMessageAsync(string message)
         {
-            var bytes = Encoding.UTF8.GetBytes(message);
-            await _webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            if (_webSocket != null && _webSocket.State == WebSocketState.Open)
+            {
+                var bytes = Encoding.UTF8.GetBytes(message);
+                await _webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
         }
     }
 }
