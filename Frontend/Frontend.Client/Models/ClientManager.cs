71c806f11f7241a77fe5cddc78932b5e12cd4339
Mustafa Ahmed <45480939+MidfieId@users.noreply.github.com>
2024-08-11 02:31:45 +0530

    deleted unused comments

--- Frontend/Frontend.Client/Models/ClientManager.cs
+++ Frontend/Frontend.Client/Models/ClientManager.cs
@@ -7,18 +7,6 @@ namespace Frontend.Client.Models
         private static readonly List<string> _users = new List<string>();
         public static event Action? OnUserListUpdate;
         public static bool IsAdmin { get; set; } = false;
-        //public static List<string> CurrentUsersList
-        //    { get { return _users; } }
         public static string CurrentUser { get; set; } = string.Empty;
-
-        //public static void UpdateUserList(string rawUserList)
-        //{
-        //    var usersList = Messaging.DecodeUserList(rawUserList);
-
-        //    _users.Clear();
-        //    _users.AddRange(usersList);
-
-        //    OnUserListUpdate?.Invoke();
-        //}
     }
 }
